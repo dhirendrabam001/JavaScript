f0670fb300f609e865736e39f3142508fcb356d4
Dhirendra Bam <103100457+dhirendrabam001@users.noreply.github.com>
2025-03-25 09:56:47 +0000

    comparsion

---
+++ 2-comparsion.js
@@ -0,0 +1,14 @@
+// console.log(2>1); //true
+// console.log(3<2); // false
+// console.log(2==1); //false
+// console.log(2!=1); // true
+
+
+// console.log("3">2);
+// console.log(4<"5");
+
+// 📌 In numeric comparisons (>, <, >=, <=), JavaScript converts strings to numbers automatically.
+
+
+
+
