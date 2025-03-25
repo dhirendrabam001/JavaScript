890c259f7be4e6f9e8ea5890a539338d869df079
dhirendrabam001 <dhirendrabam12345@gmail.com>
2025-03-25 09:46:28 -0700

    commit note

---
+++ 1_data_type_conversion.js
@@ -0,0 +1,80 @@
+let number = 100
+let num = "50asd"
+// console.log(number,typeof number);
+// console.log(num,typeof num);
+
+let finalNum = Number(num);
+// console.log(finalNum,typeof finalNum);
+
+//"33"=>33
+//"33abc"=>NAN
+// true=>1
+//false=>0
+//null=>0
+//undefined=>NaN
+
+let isBoolean = 1;
+let convertBoolean = Boolean(isBoolean);
+// console.log(convertBoolean,typeof convertBoolean);
+
+//1=>true, 0=> false
+// "" => false
+//"Dhirendra" =>True;
+
+let score = 33;
+let stringNumber = String(score);
+// console.log(stringNumber,typeof stringNumber);
+
+//String Convet number to string;
+
+//      OPERATION OF JAVASCRIPT //
+
+
+// console.log("2" + 3);
+// console.log(3 + "5");
+// console.log("2" + 2 + 2);
+
+// console.log(3 + 5 + "3");
+
+// "2" is a string and 3 is a number.
+
+// The + operator in JavaScript, when used with a string, performs string concatenation.
+
+// "2" + 3 → "23" (string).
+    //2
+//     3 is a number and "5" is a string.
+
+// Since one operand is a string, JavaScript converts 3 to a string and concatenates it.
+
+// 3 + "5" → "35" (string).
+
+   //3
+//    "2" is a string, so the first + operation is string concatenation.
+
+// "2" + 2 → "22"
+
+// "22" + 2 → "222"
+
+// Final result: "222" (string).
+
+   //4
+//    Here, 3 + 5 happens first because both are numbers (+ acts as addition).
+
+//    3 + 5 = 8
+   
+//    Then, 8 + "3" → "83" (string concatenation).
+   
+//    Final result: "83" (string) 
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 2_comparison_operator.js
@@ -0,0 +1,21 @@
+// console.log(2>1); //true
+// console.log(3<2); // false
+// console.log(2==1); //false
+// console.log(2!=1); // true
+
+
+// console.log("3">2);
+// console.log(4<"5");
+
+// 📌 In numeric comparisons (>, <, >=, <=), JavaScript converts strings to numbers automatically.
+console.log(null,typeof null);
+
+
+console.log(null>0); // false when null compared to 0 and 0>0 so called false
+console.log(null==0); // == the loose equality does not convert null to a number so null is only loose equal to undefined not a 0 so ans is false
+console.log(null<=0); // the <= operator converts 0<=0 so which is true
+
+
+
+
+
