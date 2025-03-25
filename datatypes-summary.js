f579733d39264e2b66662d38b3f59c42b9b4c4af
dhirendrabam001 <dhirendrabam12345@gmail.com>
2025-03-25 10:27:10 -0700

    Datatype-summary

---
+++ datatypes-summary.js
@@ -0,0 +1,40 @@
+// Primitive Data Types
+
+// String, Number, Boolean, Null, Undefined, Symbol,BigInt
+
+// Non Primtive Data Types[Reference data types]
+
+const id = Symbol("123")
+const anotherId = Symbol("123")
+// console.log(id===anotherId);
+
+// symbol is a built in function javascript used to unique identifiers its always return false answer
+
+// BIGINT
+const bigNumer = 123456789n;
+// console.log(typeof bigNumer);
+
+// console.log(bigNumer);
+
+
+// Arrays, Object, Function
+
+// javascript is dynamically language
+
+
+const main = ["dhirendra", "lucky", "hari", "Man"]
+let myObject= {
+    name: "dhirendra",
+    age: 21
+}
+// console.log(main);
+// console.log(myObject);
+
+// Function
+const myFunction = function() {
+    // console.log("hello world"); 
+}
+
+
+
+
