a4c0e3e3dcd837643eace675f8c8937aa73d9c33
dhirendrabam001 <dhirendrabam12345@gmail.com>
2025-03-25 09:55:32 -0700

    defined keyword

--- 2_comparison_operator.js
+++ 2_comparison_operator.js
@@ -8,12 +8,19 @@
 // console.log(4<"5");
 
 // 📌 In numeric comparisons (>, <, >=, <=), JavaScript converts strings to numbers automatically.
-console.log(null,typeof null);
+// console.log(null,typeof null);
 
 
-console.log(null>0); // false when null compared to 0 and 0>0 so called false
-console.log(null==0); // == the loose equality does not convert null to a number so null is only loose equal to undefined not a 0 so ans is false
-console.log(null<=0); // the <= operator converts 0<=0 so which is true
+// console.log(null>0); // false when null compared to 0 and 0>0 so called false
+// console.log(null==0); // == the loose equality does not convert null to a number so null is only loose equal to undefined not a 0 so ans is false
+// console.log(null<=0); // the <= operator converts 0<=0 so which is true
+
+console.log(undefined==0); // undefined is only loose equal to null but not any value so which is output always false
+console.log(undefined>0); // When JavaScript compares undefined with a number, it converts undefined to NaN (Not-a-Number).
+console.log(undefined<=0); // Any comparison with NaN is always false, including NaN <= 0
+
+
+
 
 
 
